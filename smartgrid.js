6a6fb18f2321cc4ecb5c51f74c25e1fdb3833c34
zonderweb <zonder2009@gmail.com>
2018-06-07 23:04:57 +0300

    smartgrid.js

---
+++ smartgrid.js
@@ -0,0 +1,40 @@
+const smartgrid = require('smart-grid');
+
+const settings = {
+		filename: "_smartgrid",
+		outputStyle: "scss",
+    columns: 12,
+    offset: '0px', // Межколоночный отступ
+    container: {
+        maxWidth: '1500px',
+        fields: '0px' // Внутренние отступы
+    },
+    breakPoints: {
+				bmd: {
+					width: "1170px",
+					fields: "15px"
+				},
+        md: {
+            width: "992px",
+            fields: "15px"
+        },
+        sm: {
+            width: "720px",
+            fields: "10px"
+        },
+        xs: {
+            width: "576px",
+            fields: "10px"
+        },
+        xxs: {
+            width: "380px",
+            fields: "5px"
+        }
+    },
+    oldSizeStyle: false,
+    properties: [
+        'justify-content'
+    ]
+};
+
+smartgrid ('./app/scss', settings);
