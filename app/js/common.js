7c45246984149973df3e186c08723ba7641e21a4
zonderweb <zonder2009@gmail.com>
2018-06-16 23:50:40 +0300

    tab header

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,6 @@
-$(function() {
-
-	// Custom JS
-
-});
+// MAIN TABS
+$(".tab_item").not(":first").hide();
+$(".main-tab .tab").click(function() {
+		$(".main-tab .tab").removeClass("active-tab").eq($(this).index()).addClass("active-tab");
+		$(".tab_item").hide().eq($(this).index()).fadeIn()
+}).eq(0).addClass("active-tab");
